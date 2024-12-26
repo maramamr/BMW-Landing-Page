7897a4f88c2167d22b379b96c1e058a9d8a38fe3
Maram Amr <maramamr413@gmail.com>
2024-12-26 23:50:33 +0200

    We made BMW project

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+  }, 30);
+};
